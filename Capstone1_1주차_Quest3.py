0ea2f20a41df19aa1095930d62f5fb6e213fa8ac
AlonsoOh <50190325+AlonsoOh@users.noreply.github.com>
2020-03-19 11:32:19 +0900

    Add files via upload

--- Capstone1_1주차_Quest3.py
+++ Capstone1_1주차_Quest3.py
@@ -1,10 +1,10 @@
 import requests
-import random
-import time
+import random           #랜덤함수 호출을 위함
+import time             #time.sleep()을 실행하기 위함
 
 for i in range(10):
-    value = random.randrange(1, 101)
+    value = random.randrange(1, 101)        #1부터 100까지 임의의 수 뽑음
     requests.get("https://api.thingspeak.com/update?api_key=KXUM5S9Z3JFI25VE&field1=" + str(value))
-    time.sleep(20)
+    time.sleep(20)                          #20초 정도 텀을 둠
 
  
